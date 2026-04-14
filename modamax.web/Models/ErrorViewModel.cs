6be3c154b83ec205c7b67692ea601086bc91de4c
Ëduardo <dudu_llopes@hotmail.com>
2026-04-13 23:04:21 -0300

    iniciando o projeto no vscode. estava fazendo no vs normal e nao estava gostando

---
+++ modamax.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace modamax.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
